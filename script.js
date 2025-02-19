87d2f84184c4df236c142cf4adf195fda45ef17c
dalernadzhatov <dalernadzhatov@gmail.com>
2025-02-19 18:43:51 +0300

    Add files via upload
    the for

---
+++ script.js
@@ -0,0 +1,14 @@
+for (let i = 1; i < 101 ; i++) {
+    console.log(i);
+}
+
+let names = ["Илья", "Амаль", "Ахмад", "Фаррух", "Шерик"]
+for (let index = 0; index < names.length; index++) {
+   console.log(`hi,${names[index]}`);
+}
+
+
+let text = "Text";
+for (let index = 0; index < text.length; index++) {
+    console.log(text[index]);
+}
